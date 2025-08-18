74971dcb37d0e77f446eb8b338b3f0fd74a2b2e5
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-08-18 19:34:42 +0300

    handle toast, font, global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shoplet';
+
+  constructor(private toastr: ToastrService) {}
+
+  showToast(): void {
+    this.toastr.success('Toastr is working!');
+    this.toastr.error('Toastr is working!');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 4000,
+      // extendedTimeOut: 0,
+      positionClass: 'toast-top-center',
+      progressBar: true,
+      closeButton: true,
+      tapToDismiss: true,
+      newestOnTop: true,
+    })
+  ]
 };
