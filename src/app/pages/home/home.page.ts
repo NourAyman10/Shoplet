ee9c526bdc30ff3b8a27a5ef0cb7c652ff2691ab
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-08-19 01:00:29 +0300

    set swiper image

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
-
+  images = signal<string[]>(['assets/slides/1.png', 'assets/slides/2.png', 'assets/slides/3.png']);
 }
