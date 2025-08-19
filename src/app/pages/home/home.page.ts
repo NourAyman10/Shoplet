5b4e5315fd747888f50b32a9e2a090eada04e67d
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-08-19 05:18:35 +0300

    handle category section

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { CustomImageComponent } from '../../components/custom-image/custom-image.component';
+import { SkeletonComponent } from "../../components/skeleton/skeleton.component";
+import { SectionHeaderComponent } from "../../components/section-header/section-header.component";
+import { RouterLink } from "@angular/router";
 register();
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CustomImageComponent],
+  imports: [CustomImageComponent, SkeletonComponent, SectionHeaderComponent, RouterLink],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
@@ -18,4 +21,6 @@ export class HomePage {
     'assets/slides/2.png',
     'assets/slides/33.png',
   ]);
+
+  isLoading = signal<boolean>(false);
 }
